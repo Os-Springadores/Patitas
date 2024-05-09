9c998c7ba2248a844fce8ca2c1c881e13208d7b5
Israel Bernardo <101605470+Israelsilva02@users.noreply.github.com>
2024-05-09 16:08:20 -0300

    Update ClinicaVeterinariaApplication.java

--- src/main/java/br/com/ada/patitas/ClinicaVeterinariaApplication.java
+++ src/main/java/br/com/ada/patitas/ClinicaVeterinariaApplication.java
@@ -11,4 +11,3 @@ public class ClinicaVeterinariaApplication {
 	}
 
 }
-Error creating bean with name 'veterinarioController': Unsatisfied dependency expressed through field 'veterinarioService': Error creating bean with name 'veterinarioServiceImpl': Unsatisfied dependency expressed through field 'veterinarioRepository': Error creating bean with name 'veterinarioRepository' defined in br.com.ada.patitas.repository.VeterinarioRepository defined in @EnableJpaRepositories declared on JpaRepositoriesRegistrar.EnableJpaRepositoriesConfiguration: Could not create query for public abstract java.util.List br.com.ada.patitas.repository.VeterinarioRepository.horariosDisponiveis(java.lang.Long); Reason: Failed to create query for method public abstract java.util.List br.com.ada.patitas.repository.VeterinarioRepository.horariosDisponiveis(java.lang.Long); Cannot compare left expression of type 'java.lang.String' with right expression of type 'java.lang.Long'
