757572fba4e37b3e7372021dafcd62a3986be73a
Israel Bernardo <101605470+Israelsilva02@users.noreply.github.com>
2024-05-09 16:29:02 -0300

    Update ClinicaVeterinariaApplication.java

--- src/main/java/br/com/ada/patitas/ClinicaVeterinariaApplication.java
+++ src/main/java/br/com/ada/patitas/ClinicaVeterinariaApplication.java
@@ -11,3 +11,4 @@ public class ClinicaVeterinariaApplication {
 	}
 
 }
+Error creating bean with name 'veterinarioController': Unsatisfied dependency expressed through field 'veterinarioService': Error creating bean with name 'veterinarioServiceImpl': Unsatisfied dependency expressed through field 'veterinarioRepository': Error creating bean with name 'veterinarioRepository' defined in br.com.ada.patitas.repository.VeterinarioRepository defined in @EnableJpaRepositories declared on JpaRepositoriesRegistrar.EnableJpaRepositoriesConfiguration: Could not create query for public abstract java.util.List br.com.ada.patitas.repository.VeterinarioRepository.findByConsultasNull(); Reason: Failed to create query for method public abstract java.util.List br.com.ada.patitas.repository.VeterinarioRepository.findByConsultasNull(); No property 'consultas' found for type 'Veterinario'
