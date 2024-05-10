d867fae155fc57b65d0dad9d0c091dd9768a7298
T799477 <jhenny.ticona.sis@gmail.com>
2024-05-10 15:47:06 -0300

    @jhennyticona | Criação camada repository de Paciente

--- src/main/java/java/br/com/ada/patitas/clinicaveterinaria/service/VeterinarioService.java
+++ src/main/java/java/br/com/ada/patitas/clinicaveterinaria/service/VeterinarioService.java
@@ -7,15 +7,15 @@ import java.util.Set;
 public interface VeterinarioService {
 
 
-    List<Veterinario> buscarTodosVeterinarios();
-
-    Optional<Veterinario> buscarVeterinarioPorId(Long id);
-
-    Veterinario cadastrarVeterinario(Veterinario veterinario);
-
-    void atualizarVeterinario(Long id, Veterinario veterinario);
-
-    void deletarVeterinario(Long id);
-
-    Set<Consulta> buscarConsultasPorVeterinario(Long id);
+//    List<Veterinario> buscarTodosVeterinarios();
+//
+//    Optional<Veterinario> buscarVeterinarioPorId(Long id);
+//
+//    Veterinario cadastrarVeterinario(Veterinario veterinario);
+//
+//    void atualizarVeterinario(Long id, Veterinario veterinario);
+//
+//    void deletarVeterinario(Long id);
+//
+//    Set<Consulta> buscarConsultasPorVeterinario(Long id);
 }
