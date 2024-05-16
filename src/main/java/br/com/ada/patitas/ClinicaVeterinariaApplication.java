099ca33a2f83a9fb67dd0959aae05416c367e0a6
Israel Bernardo <101605470+Israelsilva02@users.noreply.github.com>
2024-05-16 08:54:39 -0300

    Update ClinicaVeterinariaApplication.java

--- src/main/java/br/com/ada/patitas/ClinicaVeterinariaApplication.java
+++ src/main/java/br/com/ada/patitas/ClinicaVeterinariaApplication.java
@@ -13,4 +13,8 @@ public class ClinicaVeterinariaApplication {
 }
 
 
+Error starting ApplicationContext. To display the condition evaluation report re-run your application with 'debug' enabled.
+2024-05-16T08:52:50.736-03:00 ERROR 22220 --- [projetopatitas] [  restartedMain] o.s.boot.SpringApplication               : Application run failed
 
+org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'entityManagerFactory' defined in class path resource [org/springframework/boot/autoconfigure/orm/jpa/HibernateJpaConfiguration.class]: Could not determine recommended JdbcType for Java type 'br.com.ada.patitas.model.Veterinario'
+	
