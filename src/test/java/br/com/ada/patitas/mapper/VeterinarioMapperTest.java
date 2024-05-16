046c7bf587f523b726bc4baec25dea6d5ee5441d
Israel Bernardo <101605470+Israelsilva02@users.noreply.github.com>
2024-05-16 15:25:01 -0300

    @israelsilva02 testes unitarios VeterinarioMapperTest

--- src/test/java/br/com/ada/patitas/mapper/VeterinarioMapperTest.java
+++ src/test/java/br/com/ada/patitas/mapper/VeterinarioMapperTest.java
@@ -1,20 +1,59 @@
 package br.com.ada.patitas.mapper;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import br.com.ada.patitas.dto.VeterinarioDto;
+import br.com.ada.patitas.model.Veterinario;
+import java.util.ArrayList;
+import java.util.List;
 
-class VeterinarioMapperTest {
+public class VeterinarioMapperTest {
 
     @Test
-    void toDtoVeterinario() {
+    public void testToDtoVeterinario() {
+        // Given
+        Veterinario veterinario1 = new Veterinario("Dr. Carlos", "Cardiologia");
+        Veterinario veterinario2 = new Veterinario("Dra. Maria", "Dermatologia");
+        List<Veterinario> veterinarios = new ArrayList<>();
+        veterinarios.add(veterinario1);
+        veterinarios.add(veterinario2);
+
+        // When
+        List<VeterinarioDto> veterinarioDtos = VeterinarioMapper.toDtoVeterinario(veterinarios);
+
+        // Then
+        assertEquals(2, veterinarioDtos.size());
+        assertEquals("Dr. Carlos", veterinarioDtos.get(0).getNome());
+        assertEquals("Cardiologia", veterinarioDtos.get(0).getEspecialidade());
+        assertEquals("Dra. Maria", veterinarioDtos.get(1).getNome());
+        assertEquals("Dermatologia", veterinarioDtos.get(1).getEspecialidade());
     }
 
     @Test
-    void toDtoVeterinarioDto() {
+    public void testToDtoVeterinarioDto() {
+        // Given
+        Veterinario veterinario = new Veterinario("Dr. Carlos", "Cardiologia");
+
+        // When
+        VeterinarioDto veterinarioDto = VeterinarioMapper.toDtoVeterinarioDto(veterinario);
+
+        // Then
+        assertNotNull(veterinarioDto);
+        assertEquals("Dr. Carlos", veterinarioDto.getNome());
+        assertEquals("Cardiologia", veterinarioDto.getEspecialidade());
     }
 
     @Test
-    void toEntityVeterinario() {
+    public void testToEntityVeterinario() {
+        // Given
+        VeterinarioDto veterinarioDto = new VeterinarioDto("Dr. Carlos", "Cardiologia");
+
+        // When
+        Veterinario veterinario = VeterinarioMapper.toEntityVeterinario(veterinarioDto);
+
+        // Then
+        assertNotNull(veterinario);
+        assertEquals("Dr. Carlos", veterinario.getNome());
+        assertEquals("Cardiologia", veterinario.getEspecialidade());
     }
 }
