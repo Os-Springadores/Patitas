975d9ee712abd735c3d2232e91089fc0ed6364ce
Israel Bernardo <101605470+Israelsilva02@users.noreply.github.com>
2024-05-16 08:17:31 -0300

    Update ClinicaVeterinariaApplication.java

--- src/main/java/br/com/ada/patitas/ClinicaVeterinariaApplication.java
+++ src/main/java/br/com/ada/patitas/ClinicaVeterinariaApplication.java
@@ -11,8 +11,5 @@ public class ClinicaVeterinariaApplication {
 	}
 
 }
-Failed to execute goal org.apache.maven.plugins:maven-surefire-plugin:3.1.2:test (default-test) on project clinica-veterinaria: 
 
-Please refer to C:\Users\t798830\IdeaProjects\Patitas\target\surefire-reports for the individual test results.
-Please refer to dump files (if any exist) [date].dump, [date]-jvmRun[N].dump and [date].dumpstream.
 
